c127561abaf81ceaa501f98c448624d5ebd1fef1
gregvansci <gregvansci76@hotmail.com>
2022-07-09 16:59:32 -0700

    migrating back from typescript to javascript

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.css'
 import { Canvas } from '@react-three/fiber';
 
-interface Props {
-  shadowMap: boolean;
-  colorManagement: boolean;
-}
 
 function App() {
   return (
     <div className="h-auto App">
-      <Canvas camera={{ fov: 60}}> 
+      <Canvas shadowMap colorManagement camera={{ fov: 90}}> 
         <ambientLight intensity={0.5} />
         <pointLight position={[2, 3, 10]} intensity={.2}/>
         <directionalLight
+          castShadow
           position={[0, 10, 0]}
           intensity={0.5}
           shadow-mapSize-width={1024}
@@ -24,17 +21,18 @@ function App() {
           shadow-camera-top={10}
           shadow-camera-bottom={-10}
         />
-        <mesh>
+        <mesh castShadow >
           <boxBufferGeometry attach="geometry" args={[3.2, 2, 1]} />
           <meshStandardMaterial attach="material" color="#DCD7C9" />
         </mesh>
 
         <group>
-          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0,-3,0]}>
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0,-3,0]}>
             <planeBufferGeometry attach="geometry" args={[10, 10]} />
             <shadowMaterial attach='material' />
           </mesh>
         </group>
+        
       </Canvas>
     </div>
   );
